125fabd4821cd1d4b11e917eaf2d44417f67fa50
Yassin <3yassin3@gmail.com>
2017-02-18 21:54:13 +0100

    TD1

---
+++ custom.js
@@ -0,0 +1,30 @@
+function fonctionRequeteApi(url, elementID) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+					var myObj = this.responseText;
+					var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+					document.getElementById( elementID ).innerHTML = jsonPretty;
+			}
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+	
+function homePageLoading() {
+    url = 'http://bitcoin.mubiz.com/info';
+    elementID = 'info';
+    fonctionRequeteApi(url, elementID);
+		
+    url = 'http://bitcoin.mubiz.com/blockchaininfo';
+    elementID = 'blockchaininfo';
+    fonctionRequeteApi(url, elementID);
+		
+    url = 'http://bitcoin.mubiz.com/mininginfo';
+    elementID = 'mininginfo';
+    fonctionRequeteApi(url, elementID);
+		
+    url = 'http://bitcoin.mubiz.com/peerinfo';
+    elementID = 'peerinfo';
+    fonctionRequeteApi(url, elementID);
+}
